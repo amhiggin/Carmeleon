c75d9ae55da28cb2b74229843e9e2131bb397ae4
amhiggin <amhiggin@tcd.ie>
2016-03-22 15:03:24 +0000

    Trying to integrate a database for usernames and passwords into the application using phpMyAdmin (MySQL etc)

---
+++ carmeleon_login_api/config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+* Database config variables
+*/
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "root");
+define("DB_PASSWORD", "dArtagnaninKurkino12");
+define("DB_DATABASE", "carmeleon");
+?>
